973622600d934dcbfa7d380dc41d8836f5bcefba
shivam mane <shivam@mail.com>
2023-02-14 15:05:29 +0530

    added form validation for create new account form

--- src/app/main-home/create-new/create-new.component.ts
+++ src/app/main-home/create-new/create-new.component.ts
@@ -8,17 +8,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-new.component.css'],
 })
 export class CreateNewComponent implements OnInit {
-  constructor(private create: CreateNewService, private router: Router) {}
+  constructor(private create: CreateNewService, private router: Router) { }
+  invalidMsg = ''
   creat(info: any) {
+    if (info.password !== info.comfirmpassword) {
+      this.invalidMsg = 'confirm password again'
+      return
+    } else if (info.email == '' || info.password == '' || info.firstname == '' || info.lastname == '') {
+      this.invalidMsg = 'fill all fields'
+      return
+    }
+    this.invalidMsg = ''
     this.create.create(info).subscribe((data: any) => {
       if (data.toString() == 'true') {
         alert('account created successfully');
         alert('please try to log in');
         this.router.navigate(['home', 'login']);
       } else {
-        alert('please try again');
+        alert('email address already used');
       }
+    }, (err: any) => {
+      alert('please try again after some time')
     });
+
   }
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 }
